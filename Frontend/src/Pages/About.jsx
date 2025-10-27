d70218dfb1faac723df0b003e27189f8e72094e4
Tesfamicael <worknehtesfamicael707@gmail.com>
2025-10-27 00:03:43 -0700

    image format changed

--- Frontend/src/Pages/About.jsx
+++ Frontend/src/Pages/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import ProfleImage from "../assets/ProfileImage8.png";
+import ProfleImage from "../assets/profileimage8.png";
 const About = () => {
   const skillsTree = [
     {

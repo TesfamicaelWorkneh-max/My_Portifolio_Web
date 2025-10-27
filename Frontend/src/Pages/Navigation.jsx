642f0bde93c16a7711a3e1279947f068e3399edc
Tesfamicael <worknehtesfamicael707@gmail.com>
2025-10-27 07:43:38 -0700

    navigation.jsx

--- Frontend/src/Pages/Navigation.jsx
+++ Frontend/src/Pages/Navigation.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ProfileImage from "../assets/nasa2.png";
+import ProfileImage from "../assets/nasa.png";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { AnimatePresence, motion } from "framer-motion";
 import ThemeToggle from "../Components/ThemeToggle";

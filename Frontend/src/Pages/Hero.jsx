81e3e84b6c48673b98b5a1384837e35ac9b5de34
Tesfamicael <worknehtesfamicael707@gmail.com>
2025-10-27 00:55:14 -0700

    image path restored

--- Frontend/src/Pages/Hero.jsx
+++ Frontend/src/Pages/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import ProfileImage from "../assets/profileimage6.png";
+import ProfileImage from "../assets/nasa.png";
 import { FaInstagram, FaLinkedin, FaTiktok, FaYoutube } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
